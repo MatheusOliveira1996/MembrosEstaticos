ebcfc24a11c7875ccdf606b5cceae5a830a94463
MatheusOliveira1996 <matheus.hortolandia@gmail.com>
2022-09-15 17:06:42 -0300

    Add project files.

---
+++ Calculadora.cs
@@ -0,0 +1,15 @@
+﻿namespace MembrosEstaticos {
+    internal class Calculadora {
+
+        public static double Pi = 3.14;
+
+        public static double Circunferencia (double raio) {
+            return 2.0 * Pi * raio;
+        }
+
+        public static double Volume (double r) {
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
+
+    }
+}
